cfcc4f2e4b98edfc4947c566f0edb83409224b4f
maxgraffv <maksymilian.graff@gmail.com>
2024-07-20 19:41:15 +0200

    modified to accomodate hardware side of stepper motor control

--- stepperMotor.cpp
+++ stepperMotor.cpp
@@ -1,14 +1,49 @@
 #include "stepperMotor.h"
 
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-StepperMotor::StepperMotor()
+
+StepperMotor::StepperMotor(int step_pin, int dir_pin, int en_pin, 
+    int ms1_pin, int ms2_pin, 
+    MicrostepResolution microstepResolution, 
+    MotorRotationDirection rotationDirection)
+:step_pin(step_pin), dir_pin(dir_pin), en_pin(en_pin), 
+ms1_pin(ms1_pin), ms2_pin(ms2_pin), 
+microstepResolution(microstepResolution),
+rotationDirection(rotationDirection)
 {
-    std::cout << "Stepper Motor created id: "<< getId() << std::endl;
+    // Setup wiringPi
+    if (wiringPiSetup() == -1) {
+        printf("wiringPi setup failed\n");
+        exit(1);
+    }
+
+    // Set pin modes
+    pinMode(step_pin, OUTPUT);
+    pinMode(dir_pin, OUTPUT);
+    pinMode(en_pin, OUTPUT);
+    pinMode(ms1_pin, OUTPUT);
+    pinMode(ms2_pin, OUTPUT);
+
+    enable();
 }
 
 StepperMotor::~StepperMotor()
 {
-    std::cout << "Stepper Motor destroyed id: "<< getId() << std::endl;
+    // std::cout << "Stepper Motor destroyed id: "<< getId() << std::endl;
+}
+
+void StepperMotor::enable()
+{
+    digitalWrite(en_pin, LOW); // Enable pin active LOW
+}
+
+void StepperMotor::disable()
+{
+    digitalWrite(en_pin, HIGH); // Disable pin active HIGH
 }
 
 void StepperMotor::setSpeed(double speed)
@@ -18,21 +53,62 @@ void StepperMotor::setSpeed(double speed)
 
 double StepperMotor::getSpeed()
 {
+    return speed;
+}
 
+void StepperMotor::setMicrosteps( MicrostepResolution microsteps)
+{
+    switch(microsteps)
+    {
+        case MicrostepResolution::EIGHTH_STEP :
+            digitalWrite(ms1_pin, LOW);
+            digitalWrite(ms2_pin, LOW);
+            break;
+        case MicrostepResolution::SIXTEENTH_STEP :
+            digitalWrite(ms1_pin, HIGH);
+            digitalWrite(ms2_pin, HIGH);
+            break;
+        case MicrostepResolution::THIRTY_SECOND_STEP :
+            digitalWrite(ms1_pin, HIGH);
+            digitalWrite(ms2_pin, LOW);
+            break;
+        case MicrostepResolution::SIXTY_FOURTH_STEP :
+            digitalWrite(ms1_pin, LOW);
+            digitalWrite(ms2_pin, HIGH);
+            break;
+    }
+    this->microstepResolution = microsteps;
+    std::cout << "Stepper Motor "<<getId() << ": microstep resolution set to " << static_cast<int>( microsteps ) << std::endl;
+}
+
+MicrostepResolution StepperMotor::getMicrosteps()
+{
+    return microstepResolution;
 }
 
 void StepperMotor::setDirection( MotorRotationDirection direction)
 {
-    std::cout << "Stepper Motor Direction Set" << std::endl;
+    if( direction == MotorRotationDirection::CLOCKWISE)
+    {
+        digitalWrite(dir_pin, HIGH);
+        std::cout << "Stepper Motor Direction Set Clockwise" << std::endl;
+    }
+    else
+    {
+        digitalWrite(dir_pin, LOW);
+        std::cout << "Stepper Motor Direction Set Counter Clockwise" << std::endl;
+    }
 }
 
 MotorRotationDirection StepperMotor::getDirection()
 {
-
+    return rotationDirection;
 }
 
-void StepperMotor::rotate()
+void StepperMotor::step()
 {
-    std::cout << "Motor Rotating" << std::endl;
+    digitalWrite(step_pin, HIGH);
+    delayMicroseconds(stepDelay_microsec);  // Delay for the step
+    digitalWrite(step_pin, LOW);
+    delayMicroseconds(stepDelay_microsec);  // Delay before next step
 }
-
--- stepperMotor.h
+++ stepperMotor.h
@@ -5,23 +5,54 @@
 #include "motor.h"
 #include "microstepResolutionEnum.h"
 
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define OUTPUT 1
+#define INPUT 0
+#define LOW 0
+#define HIGH 1
+
+
 class StepperMotor : public Motor
 {
     private:
-        Axis axis;
+        int step_pin, dir_pin, en_pin, ms2_pin, ms1_pin;
         MicrostepResolution microstepResolution;
+        MotorRotationDirection rotationDirection;
+        double speed;
+        double stepDelay_microsec;
 
-
+        /*
+            ms2 ms1 microstep        
+            GND GND     8  | YES it's 8
+            VCC VCC     16 | YES it's 16
+            GND VCC     2 | nope it's actually 32
+            VCC GND     4 | nope its actually 64
+        */
 
     public:
-        StepperMotor();
+        StepperMotor(int step_pin, int dir_pin, int en_pin, int ms1_pin, int ms2_pin, MicrostepResolution MicrostepResolution, MotorRotationDirection rotationDirection);
         virtual ~StepperMotor();
         
+        void enable();
+        void disable();
+
         void setSpeed(double speed) override;
         double getSpeed() override;
+    
+        void setMicrosteps( MicrostepResolution microsteps);
+        MicrostepResolution getMicrosteps();
+
         void setDirection( MotorRotationDirection ) override;
         MotorRotationDirection getDirection() override;
-        void rotate() override;
+
+        void setStepDelayMicrosec( double microseconds );
+        double getStepDelayMicrosec();
+
+        void step();
 
 };
 
