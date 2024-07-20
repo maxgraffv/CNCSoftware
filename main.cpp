e1f83007dfb416be30db60b4dc902b3b76c2f3d2
maxgraffv <maksymilian.graff@gmail.com>
2024-07-20 19:46:26 +0200

    RPi3 GPIO pins inserted

--- main.cpp
+++ main.cpp
@@ -4,26 +4,28 @@
 #include "spindle.h"
 #include "gcodeFile.h"
 #include "unitsEnum.h"
+#include "microstepResolutionEnum.h"
+#include "MotorRotationDirectionEnum.h"
+#include "rpi3pinout.h"
 
 
 
 int main()
 {
 
-    StepperMotor motorAxisZ;
-    StepperMotor motorAxisY_1;
-    StepperMotor motorAxisY_2;
-    StepperMotor motorAxisX;
-    Spindle spindle;
+    StepperMotor motorAxisZ( GPIO17 , GPIO27 , GPIO22, GPIO14, GPIO15, MicrostepResolution::EIGHTH_STEP, MotorRotationDirection::CLOCKWISE);
+    StepperMotor motorAxisY_1(GPIO23 , GPIO24, GPIO10, GPIO9, GPIO11, MicrostepResolution::EIGHTH_STEP, MotorRotationDirection::CLOCKWISE);
+    StepperMotor motorAxisY_2( GPIO25, GPIO8, GPIO7, GPIO5, GPIO6, MicrostepResolution::EIGHTH_STEP, MotorRotationDirection::CLOCKWISE);
+    StepperMotor motorAxisX( GPIO13, GPIO19, GPIO16, GPIO26, GPIO20, MicrostepResolution::EIGHTH_STEP, MotorRotationDirection::CLOCKWISE);
+    Spindle spindle(1000, 1000);
 
 
 
     CNCSetup myCNC( motorAxisX, motorAxisY_1, motorAxisY_2, motorAxisZ, spindle, Units::milimeter );
-    GCodeFile operation1( "101example.ngc" );
+    GCodeFile operation1( "101exampleSHORT.ngc" );
     myCNC.run(operation1);
 
 
-    // operation1.print();
     
 
 
