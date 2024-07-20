80b129aa7854c523fc468d733a36d7419fac7a49
maxgraffv <maksymilian.graff@gmail.com>
2024-07-20 16:28:48 +0200

    created motion enums

---
+++ motionPlaneEnum.h
@@ -0,0 +1,11 @@
+#ifndef MOTION_PLANE_ENUM_H
+#define MOTION_PLANE_ENUM_H
+
+enum class MotionPlane{
+
+    XY = 17, XZ = 18, YZ = 19 //assigned value is a gcode value
+
+};
+
+
+#endif
---
+++ motionTypeEnum.h
@@ -0,0 +1,13 @@
+#ifndef MOTION_TYPE_ENUM_H
+#define MOTION_TYPE_ENUM_H
+
+enum class MotionTypeEnum
+{
+    RapidPositioning = 0, LinearInterpolation = 1, 
+    CircularInterpolationClockwise = 3,
+    CircularInterpolationCounterClockwise = 4
+};
+
+
+
+#endif
