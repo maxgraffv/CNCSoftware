3435a259a3bf481feabc9d576cd04b9848142ff3
maxgraffv <maksymilian.graff@gmail.com>
2024-07-21 16:12:47 +0200

    rotation direction inversion when negative travel distance FIX

--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@ int main()
 
 
     CNCSetup myCNC( motorAxisX, motorAxisY_1, motorAxisY_2, motorAxisZ, spindle, Units::milimeter );
-    GCodeFile operation1( "101example.ngc" );
+    GCodeFile operation1( "manual.ngc" );
     myCNC.run(operation1);
 
 
