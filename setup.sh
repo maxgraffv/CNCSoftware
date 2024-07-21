aacc1f7ce9ddd568b49fc4f7a3f2ad9638614ce9
maxgraffv <maksymilian.graff@gmail.com>
2024-07-21 15:23:57 +0200

    bash script updated to do repository pull

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
+echo "repository pulling...\n"
+git pull origin main
+echo "repository pulled\n"
 
+echo "GPIO check...\n"
 gpio -v
 
+echo "compiling...\n"
 g++ *.cpp -o main -lwiringPi -std=c++20
+echo "compiled\n"
 
 #./main
