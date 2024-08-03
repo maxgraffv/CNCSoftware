5b839a48de5761ca8c1ac04fc4adf4ef1b761065
maxgraffv <maksymilian.graff@gmail.com>
2024-08-04 00:45:42 +0200

    OS boot up logo service added

---
+++ boot.sh
@@ -0,0 +1,10 @@
+echo "fbi install...\n"
+
+sudo apt update
+sudo apt install fbi
+
+#move logoscreen.service to /etc/systemd/system
+
+#sudo systemctl enable logoscreen.service
+
+#removed splash from boot/firmware/cmdline.txt
