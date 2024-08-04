5cc260885850823e33dee114ce4d708ac1593ddc
maxgraffv <maksymilian.graff@gmail.com>
2024-08-04 14:15:42 +0200

    vim, git installs added; fixed splash_screen.sh logo path

--- install.sh
+++ install.sh
@@ -1,22 +1,47 @@
-echo "WiringPi installing...\n"
+echo "ssh enabling..."
+echo ""
+sudo systemctl enable ssh
+sudo systemctl start ssh
+sudo systemctl status ssh
+echo ""
+echo ""
+
+echo -e "git installing...\n"
+sudo apt install git
+echo ""
+echo ""
+
+echo -e "vim installing...\n"
+sudo apt install git
+echo ""
+echo ""
+
+
+echo -e "WiringPi installing...\n"
 cd 
 git clone https://github.com/WiringPi/WiringPi.git
 cd WiringPi
 ./build
 gpio -v
 cd 
-echo "\n\n"
+echo ""
+echo ""
 
 
-echo "fbi installing...\n"
+echo -e "fbi installing...\n"
 sudo apt install fbi
-echo "\n\n"
+echo ""
+echo ""
 
 
-echo "splashscreen service...\n"
+echo -e "splashscreen service...\n"
 sudo mv CNCSoftware/splashScreen/splashScreen.service /etc/systemd/system/
 sudo mv CNCSoftware/splashScreen/show_splash.sh /usr/local/bin/
 sudo systemctl enable splashScreen.service
-echo "\n\n"
+echo ""
+echo ""
 
+
+echo -e "Rebooting...\n"
+sudo reboot
 
